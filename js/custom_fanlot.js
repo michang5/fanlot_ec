0aba20ba5fbf5af1ec0e4986ae9a54c24edb626a
MiChang <immi5.tw@gmail.com>
2016-04-11 09:38:45 +0800

    start ecommerce - navigation

--- js/custom_fanlot.js
+++ js/custom_fanlot.js
@@ -5,25 +5,25 @@ $(function () {
         $(this).toggleClass('nav-close');
     });
 
-    $(".nav li a[href^='#'], .logo-s a, .logo a").on('click', function (e) {
-        e.preventDefault();
-        var hash = this.hash;
-
-        $('html, body').animate({
-            scrollTop: $(hash).offset().top
-        }, 300, function () {
-            window.location.hash = hash;
-        });
-    });
-    var mainbottom = $('#Carousel').offset().top + $('#Carousel').height();
+//    $(".nav li a[href^='#'], .logo-s a, .logo a").on('click', function (e) {
+//        e.preventDefault();
+//        var hash = this.hash;
+//
+//        $('html, body').animate({
+//            scrollTop: $(hash).offset().top
+//        }, 300, function () {
+//            window.location.hash = hash;
+//        });
+//    });
+    var mainbottom = $('#page-header').offset().top + $('#page-header').height();
 
     $(window).on('scroll', function () {
         stop = Math.round($(window).scrollTop());
         if (stop > mainbottom) {
-            $('.navbar').addClass('past-main');
+            $('#top-header').addClass('past-main');
             
         } else {
-            $('.navbar').removeClass('past-main');
+            $('#top-header').removeClass('past-main');
             
         }
     });
